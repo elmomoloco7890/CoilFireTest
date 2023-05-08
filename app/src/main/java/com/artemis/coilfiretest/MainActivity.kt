9b572041d543b8ef16ac87d032a49d2239ce2962
elmomoloco7890 <thegreek.god@gmail.com>
2023-05-08 00:16:39 -0700

    The beginnings for storage and data

--- app/src/main/java/com/artemis/coilfiretest/MainActivity.kt
+++ app/src/main/java/com/artemis/coilfiretest/MainActivity.kt
@@ -2,10 +2,43 @@ package com.artemis.coilfiretest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.artemis.coilfiretest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val actBinding = ActivityMainBinding.inflate(layoutInflater)
+        binding = actBinding
+        setContentView(binding.root)
+
+        //toolbar
+        val toolBar = binding.toolBar
+        toolBar.title = ""
+        toolBar.clearNavigationIconTint()
+        setSupportActionBar(binding.toolBar)
+
+        // Retrieve NavController from the NavHostFragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.coil_nav_host) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Set up the action bar for use with the NavController
+        setupActionBarWithNavController(navController)
+    }
+
+    /**
+     * Handle navigation when the user chooses Up from the action bar.
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/artemis/coilfiretest/fragments/CoilMain.kt
@@ -0,0 +1,23 @@
+package com.artemis.coilfiretest.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemis.coilfiretest.R
+
+
+class CoilMain : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_coil_main, container, false)
+    }
+
+
+}
